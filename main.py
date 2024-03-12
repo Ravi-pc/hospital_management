929db350176ad76f2797f99432bf09778bae2629
“Ravi-pc” <ravisingh98138@gmail.com>
2024-03-12 15:08:10 +0530

    [Ravi] Add: Program for the doctor Api with changes in schema.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from sanic import Sanic
-from sanic.response import text
+
+from routes.department_api import department_api
 from routes.doctor_api import doctor_api
 
 app = Sanic('HospitalManagement')
 
 app.blueprint(doctor_api)
-
+app.blueprint(department_api)
